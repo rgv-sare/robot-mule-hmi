d2ac0cbbf71f3a763e2311d0708b34b80734ce7b
Jesus Hernandez <jesushdz2002ch14@gmail.com>
2024-04-19 15:05:09 -0500

    edit home screen

--- src/Index/render.js
+++ src/Index/render.js
@@ -1,3 +1,7 @@
 document.getElementById("manual-control").addEventListener("click", function() {
     window.location.href = "../Camera (Manual Control)/camera.html";
     });
+
+document.getElementById("track").addEventListener("click", function() {
+    window.location.href = "../Track/track.html";
+    });
